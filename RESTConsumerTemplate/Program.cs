11d92db47e7facb7e1efefc8511633c631b41c63
Mikail Fener <mika1292@edu.easj.dk>
2019-06-12 10:16:14 +0200

    Add project files.

---
+++ RESTConsumerTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RESTConsumerTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
